c6cb2709990eb2617694b1a31f3b5292cd706039
Lordwick5 <Prashantchy96@gmail.com>
2025-04-14 19:37:10 +0545

    updated code

--- index.js
+++ index.js
@@ -24,7 +24,30 @@ next.onclick = function (){
   reloadSlider();
 }
 
+prev.onclick = function(){
+  if(active - 1 < 0){
+    active = lengthItems;
+  }else {
+    active = active - 1;
+  }
+  reloadSlider();
+}
+
+let refreshSlider = setInterval(() => {next.click()}, 3000);
 function reloadSlider() {
   let checkLeft = items[active].offsetLeft;
   list.style.left = -checkLeft + 'px';
+
+  let lastActiveDot = document.querySelector('.slider .dots li.active');
+  lastActiveDot.classList.remove('active');
+  dots[active].classList.add('active');
+  clearInterval(refreshSlider);
+  refreshSlider = setInterval(() => {next.click()}, 3000);
 }
+
+dots.forEach((li, key) => {
+  li.addEventListener('click', function(){
+    active = key;
+    reloadSlider();
+  })
+})
