7e24bf38b57b6a4bcd3fbd1c0b3b67546f7e49ac
Lordwick5 <Prashantchy96@gmail.com>
2025-04-14 19:07:40 +0545

    updated js

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const toggleBtn = document.getElementById('menu-toggle');
   
 });
 
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
