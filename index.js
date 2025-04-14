64119bcdc6f205805873f1dd2d3e39ceb2f35268
Lordwick5 <Prashantchy96@gmail.com>
2025-04-14 19:14:54 +0545

    updated js code

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ let prev = document.getElementById('prev');
 let next = document.getElementById('next');
 
 let active = 0;
+let lengthItems = items.length;
+
+next.onclick = function (){
+  if(active + 1 > lengthItems) {
+    active = 0;
+  }else{
+    active = active + 1;
+  }
+  reloadSlider();
+}
+
+function reloadSlider() {
+  let checkLeft = items[active].offsetLeft;
+  list.style.left = -checkLeft + 'px';
+}
